55942ad978053ff224de99ab3b12f8fbbe29a502
daniellecroxton <danikrist@gmail.com>
2021-01-19 15:22:50 -0700

    Ran create-react-app and removed unnecessary files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div className="App">
+          <p>Welcome to needful. A work in progress.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
