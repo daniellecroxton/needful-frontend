1f85bf225d33a5e310a18236da148d756ae334ed
daniellecroxton <danikrist@gmail.com>
2021-01-21 15:57:24 -0700

    added fetch request to test data coming through from needful-backend

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 class App extends React.Component {
 
+  componentDidMount() {
+
+    fetch('http://localhost:3000/api/v1/users/2/items')
+    .then(response => response.json())
+    .then(res => console.log(res))
+  }
+
   render() {
 
     return (
