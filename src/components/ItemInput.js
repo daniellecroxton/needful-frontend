f935fecac66bb54af5f24f2376a840b3c54e2cbc
daniellecroxton <danikrist@gmail.com>
2021-01-25 19:27:53 -0700

    Started item input form

--- src/components/ItemInput.js
+++ src/components/ItemInput.js
@@ -1,15 +1,40 @@
 import React from 'react'
-
+import {connect} from 'react-redux'
 class ItemInput extends React.Component {
+
+state = {
+    name: '',
+    description: '',
+    price: ''
+}
+
+handleChange = (event) => {
+    this.setState({
+        [event.target.name]: event.target.value
+    })
+}
+
+handleSubmit = (event) => {
+    
+}
 
     render() {
 
         return (
             <div>
                 ItemInput
+                <form onSubmit={this.handleSubmit}>
+                    <label>Item Name:</label>
+                    <input type='text' name="name" value={this.state.name} onChange={this.handleChange}></input>
+                    <label>Item Description:</label>
+                    <input type='text' name="description" value={this.state.description} onChange={this.handleChange}></input>
+                    <label>Item Price:</label>
+                    <input type='text' name="price" value={this.state.price} onChange={this.handleChange}></input>
+                    <input type='submit'></input>
+                </form>
             </div>
         )
     }
 }
 
-export default ItemInput
+export default connect(null)(ItemInput)
