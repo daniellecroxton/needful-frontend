1227daa6f6e91f5d052bf6952d5738de1cc30851
daniellecroxton <danikrist@gmail.com>
2021-01-25 21:42:45 -0700

    updated user item list to pass in correct argument

--- src/components/UserItemList.js
+++ src/components/UserItemList.js
@@ -5,14 +5,14 @@ import {updateItem} from '../actions/updateItem'
 const UserItemList = (props) => {
 
     const handleUpdate = (item) => {
-        props.updateItem(item.id, item.user.id)
+        props.updateItem(item)
     }
 
     return (
         <div>
             UserItem List
             {props.items && props.items.map(item =>
-                <li key={item.id}>{item.name} - {item.price} <button onClick={() => handleUpdate(item)}/>Update</li>
+                <li key={item.id}>{item.name} - {item.price} <button onClick={() => handleUpdate(item)}>Update</button></li>
                 )}
         </div>
     )
